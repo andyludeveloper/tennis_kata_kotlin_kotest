176b078b08d727e2f6be78414a06a2a0f570bead
Andy Lu <alu@futuredial.com>
2020-12-27 00:27:21 +0800

    refactor: extract playerScore

--- src/test/kotlin/TennisTest.kt
+++ src/test/kotlin/TennisTest.kt
@@ -1,17 +1,29 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class TennisTest : FunSpec(
-    {
+
+
+class TennisTest : FunSpec() {
+    private val sut = Tennis()
+   init {
         test("test love all"){
-            val sut = Tennis()
+            player1Score(0)
             sut.score() shouldBe "love_all"
         }
+
         test("test fifteen love"){
-            val sut = Tennis()
-            sut.firstPlayerScore()
+            player1Score(1)
             sut.score() shouldBe "fifteen_love"
         }
-
+        test("test thirty love"){
+            player1Score(2)
+            sut.score() shouldBe "thirty_love"
+        }
     }
-)
+
+    private fun player1Score(i: Int) {
+        for(score in 0 until i)
+            sut.firstPlayerScore()
+    }
+}
+
