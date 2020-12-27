83327dac64150c465da698d85cdd5a4132c4647b
Andy Lu <alu@futuredial.com>
2020-12-27 22:14:58 +0800

    refactor

--- src/main/kotlin/Tennis.kt
+++ src/main/kotlin/Tennis.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 class Tennis(val player1Name:String, val player2Name:String) {
     var player1Score = 0
     var player2Score = 0
@@ -15,13 +17,11 @@ class Tennis(val player1Name:String, val player2Name:String) {
             }
             return "${stringLookup[player1Score]}_all"
         }
-
-        if(player1Score >= 3 && player1Score - player2Score == 1) {
-            return "$player1Name adv"
-        }
-
-        if(player2Score >= 3 && player2Score - player1Score == 1) {
-            return "$player2Name adv"
+        if(player1Score >= 3 && player2Score >= 3 ) {
+             if(abs(player1Score - player2Score) == 1) {
+                 val advName = if(player1Score>player2Score) player1Name else player2Name
+                 return "$advName adv"
+             }
         }
 
         return "${stringLookup[player1Score]}_${stringLookup[player2Score]}"
