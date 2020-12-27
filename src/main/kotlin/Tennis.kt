37f3432b016bf0b78690fad55e8d44a14c55e118
Andy Lu <alu@futuredial.com>
2020-12-27 22:03:02 +0800

    test "player 2 adv": pass

--- src/main/kotlin/Tennis.kt
+++ src/main/kotlin/Tennis.kt
@@ -16,8 +16,12 @@ class Tennis(val player1Name:String, val player2Name:String) {
             return "${stringLookup[player1Score]}_all"
         }
 
-        if(isDuece() && player1Score - player2Score == 1) {
+        if(player1Score >= 3 && player1Score - player2Score == 1) {
             return "$player1Name adv"
+        }
+
+        if(player2Score >= 3 && player2Score - player1Score == 1) {
+            return "$player2Name adv"
         }
 
         return "${stringLookup[player1Score]}_${stringLookup[player2Score]}"
