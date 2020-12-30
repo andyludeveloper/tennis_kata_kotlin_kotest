10078df1034f0e3d69c7fc02bbf122068fc29491
Andy Lu <alu@futuredial.com>
2020-12-30 13:28:00 +0800

    test "forty_love": pass

--- src/main/kotlin/Tennis.kt
+++ src/main/kotlin/Tennis.kt
@@ -2,7 +2,8 @@ class Tennis {
     private var player1Score: Int = 0
     private val scoreLookup = mapOf(
             1 to "fifteen",
-            2 to "thirty"
+            2 to "thirty",
+            3 to "forty"
     )
     fun score(): String {
         if (player1Score > 0) {
