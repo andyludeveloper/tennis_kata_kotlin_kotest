4cc57103c8dd6f08bf75999fe7f6ef66ca67cd66
Andy Lu <alu@futuredial.com>
2020-12-30 13:24:38 +0800

    refactor: extract scoreLookup map table

--- src/main/kotlin/Tennis.kt
+++ src/main/kotlin/Tennis.kt
@@ -1,12 +1,12 @@
 class Tennis {
     private var player1Score: Int = 0
-
+    private val scoreLookup = mapOf(
+            1 to "fifteen",
+            2 to "thirty"
+    )
     fun score(): String {
-        if (player1Score == 1) {
-            return "fifteen_love"
-        }
-        if(player1Score == 2){
-            return "thirty_love"
+        if (player1Score > 0) {
+            return "${scoreLookup[player1Score]}_love"
         }
         return "love_all"
     }
