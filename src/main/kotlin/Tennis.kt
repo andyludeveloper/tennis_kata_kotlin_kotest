1379b8f53862f03d612d46c2d7aac447e852b395
Andy Lu <alu@futuredial.com>
2020-12-30 13:22:45 +0800

    test: "thirty_love" : pass

--- src/main/kotlin/Tennis.kt
+++ src/main/kotlin/Tennis.kt
@@ -5,6 +5,9 @@ class Tennis {
         if (player1Score == 1) {
             return "fifteen_love"
         }
+        if(player1Score == 2){
+            return "thirty_love"
+        }
         return "love_all"
     }
 
